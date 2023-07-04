1e4e2ebd648cfce80d7464a40ac9d3e7684dee0b
Anders <ansol@dtu.dk>
2023-07-04 14:01:17 +0200

    fix

--- src/models_pytorch/mixture_torch_loop.py
+++ src/models_pytorch/mixture_torch_loop.py
@@ -13,8 +13,8 @@ def mixture_torch_loop(model,data,tol=1e-8,max_iter=10000,num_repl=1,init='no',L
         # the 'no'-option (default) is for ACG-lowrank, where some columns are randomly initialized and others prespecified
         
         optimizer = torch.optim.Adam(model.parameters(),lr=LR)
-        # scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer,threshold=tol,threshold_mode='abs',min_lr=0.0001,patience=100)
-        scheduler = None
+        scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer,threshold=tol,threshold_mode='abs',min_lr=0.0001,patience=100)
+        # scheduler = None
 
         loglik = []
 
