d3cd2ccb8c728e978c2199f655642cbac3b81093
Anders <ansol@dtu.dk>
2023-08-04 15:02:29 +0200

    fix

--- submitfiles/submit_pyspheremixtures454.sh
+++ submitfiles/submit_pyspheremixtures454.sh
@@ -5,6 +5,7 @@ do
 for m in 0 1 2
 do
 for K in {2..30}
+do
 
     sed -i '$ d' submitfiles/pyspheremixtures454_template.sh
     echo "python3 experiments/run_models_454.py $m $LR ++ $K" >> submitfiles/pyspheremixtures454_template.sh
