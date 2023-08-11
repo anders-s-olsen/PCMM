cb4642cfcc4e163961d6d3c0b944c7b5ef5b6f9f
anders-s-olsen <ansol@dtu.dk>
2023-08-11 09:37:02 +0200

    results

--- submitfiles/pyspheremixtures454_template.sh
+++ submitfiles/pyspheremixtures454_template.sh
@@ -15,4 +15,4 @@
 source /dtu-compute/macaroni/miniconda3/bin/activate
 conda activate hcp
 
-python3 experiments/run_models_116.py MACG 0.1 ++ 1
+python3 experiments/run_models_116.py MACG 0.01 ++ 1
