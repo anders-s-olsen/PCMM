8a9f0e85e7fad7e7c85f36aa6451ca69f8ac2ab3
anders-s-olsen <ansol@dtu.dk>
2023-08-14 09:55:42 +0200

    results

--- src/data/fMRI_compute_eigs.m
+++ src/data/fMRI_compute_eigs.m
@@ -3,7 +3,7 @@ maxNumCompThreads('automatic');
 rng shuffle
 subjects = dir('/dtu-compute/HCP_dFC/2023/hcp_dfc/data/raw');
 
-perform_GSR = false;
+perform_GSR = true;
 
 % Compute eigenvectors
 TR = 0.72;%s
@@ -20,21 +20,22 @@ for sub = randperm(numel(subjects))
     dses = dir([subjects(sub).folder,'/',subjects(sub).name,'/fMRI/rfMRI_REST*_RL*']);
     for ses = 1:numel(dses)
         tic
-        if perform_GSR
-            if exist(['/dtu-compute/HCP_dFC/2023/hcp_dfc/data/processed/fMRI_SchaeferTian116_GSR/',subjects(sub).name,'_',dses(ses).name(1:end-13),'.mat'])
-                disp('continue')
-                continue
-            end
-        else
-            if exist(['/dtu-compute/HCP_dFC/2023/hcp_dfc/data/processed/fMRI_SchaeferTian116/',subjects(sub).name,'_',dses(ses).name(1:end-13),'.mat'])
-                disp('continue')
-                continue
-            end
-        end
+%         if perform_GSR
+%             if exist(['/dtu-compute/HCP_dFC/2023/hcp_dfc/data/processed/fMRI_SchaeferTian116_GSR/',subjects(sub).name,'_',dses(ses).name(1:end-13),'.mat'])
+%                 disp('continue')
+%                 continue
+%             end
+%         else
+%             if exist(['/dtu-compute/HCP_dFC/2023/hcp_dfc/data/processed/fMRI_SchaeferTian116/',subjects(sub).name,'_',dses(ses).name(1:end-13),'.mat'])
+%                 disp('continue')
+%                 continue
+%             end
+%         end
         disp(['Working on subject ',subjects(sub).name,' session ',num2str(ses)])
-        data = double(squeeze(niftiread([dses(ses).folder,'/',dses(ses).name])));
+        data = detrend(double(squeeze(niftiread([dses(ses).folder,'/',dses(ses).name]))));
+        
         if perform_GSR
-            GS = detrend(mean(data,2));
+            GS = mean(data(:,1:59412),2);
             data = data-GS.*(data'*GS)'/(GS'*GS);
         end
         %         disp(['Loaded data in ',num2str(toc),' seconds'])
@@ -43,7 +44,7 @@ for sub = randperm(numel(subjects))
         eigenvectors_roi = nan(size(data,1)*2,max(atlas(:)));
         for roi = 1:max(atlas(:))
             data_roi(:,roi) = mean(data(:,atlas==roi),2);
-            data_roi(:,roi) = angle(hilbert(filtfilt(bfilt,afilt,detrend(data_roi(:,roi)))));
+            data_roi(:,roi) = angle(hilbert(filtfilt(bfilt,afilt,data_roi(:,roi))));
         end
         if any(isnan(data_roi(:)))
             error('nan reached')
@@ -66,28 +67,7 @@ for sub = randperm(numel(subjects))
         else
             parSave(['/dtu-compute/HCP_dFC/2023/hcp_dfc/data/processed/fMRI_SchaeferTian116/',subjects(sub).name,'_',dses(ses).name(1:end-13),'.mat'],eigenvectors_roi)
         end
-        %         Phase_BOLD = nan(size(data));
-        %         eigenvectors_all = nan(size(data));
-        %
-        %         for i = 1:size(data,2)
-        %             Phase_BOLD(:,i) = angle(hilbert(filtfilt(bfilt,afilt,detrend(data(:,i)))));
-        %         end
-        % %         disp(['data Hilbert done in ',num2str(toc),' seconds'])
-        %
-        %         for tt = 1:size(data,1)
-        %
-        %             cosX = cos(Phase_BOLD(tt,:));
-        %             sinX = sin(Phase_BOLD(tt,:));
-        %
-        %             y = Phase_BOLD(tt,:)';
-        %             X = [cosX',sinX'];
-        %
-        %             [U,~,~] = svds(X,2);
-        %             eigenvectors_all(tt*2-1:tt*2,:) = U;
-        %         end
-        %         disp(['Data eig done in ',num2str(toc),' seconds'])
-        %
-        %         parSave(['/dtu-compute/HCP_dFC/2023/hcp_dfc/data/processed/fMRI_full/',subjects(sub).name,'_',dses(ses).name(1:end-13),'.mat'],eigenvectors_all)
+                
 
     end
 end
