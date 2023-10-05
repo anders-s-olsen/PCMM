757b07f9d0ad228624398f6e7a28b8346ed11ba4
Anders <ansol@dtu.dk>
2023-10-05 17:18:28 +0200

    fix

--- submitfiles/HPC_template_8threads_100MB.sh
+++ submitfiles/HPC_template_8threads_100MB.sh
@@ -6,7 +6,7 @@
 # #BSUB -N
 #BSUB -o submitfiles/output/synthjob_out_%J.txt
 #BSUB -e submitfiles/output/synthjob_err_%J.txt
-#BSUB -W 24:00 
+#BSUB -W 72:00 
 #BSUB -n 8
 #BSUB -R "span[hosts=1]"
 
