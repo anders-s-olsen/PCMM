25edf233ea4b775ff9cd879a7bb3d02ba50b74a7
Anders <ansol@dtu.dk>
2023-10-05 11:01:53 +0200

    fix

--- submitfiles/submit_pyspheremixtures_synth.sh
+++ submitfiles/submit_pyspheremixtures_synth.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 inits=("unif" "++" "dc")
-for LR in 0 0.001 0.01 0.1
+modelnames=("Watson" "ACG" "MACG")
+for LR in 0 0.001 0.01 0.1 1
 do
 for init in "${inits[@]}"
 do
-for m in 1 2
+for m in "${modelnames[@]}"
 do
 
     sed -i '$ d' submitfiles/pyspheremixtures_synth_template.sh
