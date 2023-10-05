3ac7afad6d103f6a2f4fe8e68f53bef46df6dd82
Anders <ansol@dtu.dk>
2023-10-05 15:32:08 +0200

    analysis

--- experiments/analyses/analyze_loglikcurves_synth.py
+++ experiments/analyses/analyze_loglikcurves_synth.py
@@ -10,23 +10,21 @@ sns.set_style("whitegrid", {'axes.grid' : False})
 
 num_repl_outer = 10
 inits = ['unif','++','dc']
-LRs = [0.001,0.01,0.1,0]
+LRs = [0,0.001,0.01,0.1,1]
 exptype = 'synth'
 for m in range(3):
-    if m==0:
-        continue
     for tt in range(2): # train test likelihood
         fig, axs = plt.subplots(3, 3, figsize=(16, 9))
         if m==0:
-            name='Watson'
+            modelname='Watson'
         elif m==1:
-            name='ACG'
+            modelname='ACG'
         elif m==2:
-            name = 'MACG'
+            modelname = 'MACG'
         # left_labels = ['Number of components, K=2','Number of components, K=5','Number of components, K=10']
         for idx2,p in enumerate([3,10,25]):
             for idx1,K in enumerate([2,5,10]):
-                if K>p:
+                if K>=p:
                     axs[idx1,idx2].set_visible(False)
                     continue
                 df = pd.DataFrame(columns=['Log likelihood','model','Optimizer','Initialization'])
@@ -37,20 +35,23 @@ for m in range(3):
                         else:
                             LRname = str(LR)
                         expname = '3d_'+init+'_'+LRname+'_p'+str(p)+'_K'+str(K)
-                        
+                        file_path = 'data/results/'+exptype+'_outputs/'+modelname+'_'+expname+'_traintestlikelihood.csv'
+                        try:
+                            likes = np.loadtxt(file_path)
+                        except:
+                            continue
+
                         testlike = []
-                        modelname = []
+                        modelnames = []
                         LRval = []
                         initval = []
                         for rep in range(num_repl_outer):
-                            file_path = 'experiments/'+exptype+'_outputs/'+name+'_'+expname+'_traintestlikelihood_r'+str(rep)+'.csv'
-                            try:
-                                like = np.loadtxt(file_path)[tt]
-                                # if like < -1800:
-                                #     raise ValueError
-                                testlike.append(like)
-                            except: continue
-                            modelname.append(name)
+                            
+                            like = likes[tt,rep]
+                            if like==0:
+                                like=np.nan
+                            testlike.append(like)
+                            modelnames.append(modelname)
                             if LR==0:
                                 LRval.append('EM')
                             else:
@@ -61,16 +62,22 @@ for m in range(3):
                                 initval.append('DC++')
                             elif init == 'dc':
                                 initval.append('DC')
-                        dftmp = pd.DataFrame({'Log likelihood': testlike, 'model': modelname, 'Optimizer': LRval, 'Initialization': initval})
+                        dftmp = pd.DataFrame({'Log likelihood': testlike, 'model': modelnames, 'Optimizer': LRval, 'Initialization': initval})
                         df = pd.concat([df,dftmp])
-                sns.violinplot(ax=axs[idx1,idx2],data=df[df.model==name],x='Initialization',y='Log likelihood',hue='Optimizer',inner='point',scale='count')
-                if not(idx1==0 and idx2==2):
-                    axs[idx1,idx2].legend([],[], frameon=False)
+                try:
+                    sns.violinplot(ax=axs[idx1,idx2],data=df[df.model==modelname],x='Initialization',y='Log likelihood',hue='Optimizer',inner='point',scale='count')
+                except:
+                    continue
+                
+                if idx1!=0 or idx2!=0:
+                    if m==0 and tt==0:
+                        axs[idx1,idx2].legend([],[], frameon=False)
+                    
                 # axs[idx1,idx2].grid(False)
                 if tt == 0:
-                    axs[idx1,idx2].set_title(name+' synthetic data training log-likelihood, p='+str(p)+' K='+str(K), fontsize=12)
+                    axs[idx1,idx2].set_title(modelname+' synthetic train log-likelihood, p='+str(p)+' K='+str(K), fontsize=12)
                 elif tt == 1:
-                    axs[idx1,idx2].set_title(name+' synthetic data test log-likelihood, p='+str(p)+' K='+str(K), fontsize=12)
+                    axs[idx1,idx2].set_title(modelname+' synthetic test log-likelihood, p='+str(p)+' K='+str(K), fontsize=12)
                 axs[idx1,idx2].set_xlabel('')
                 axs[idx1,idx2].get_yaxis().set_major_formatter(
                     ticker.FuncFormatter(lambda x, p: format(x)))
@@ -81,7 +88,7 @@ for m in range(3):
 
         # Adjust the spacing between subplots
         fig.tight_layout()
-        plt.savefig('reports/figures/synth_results_'+name+'_'+str(tt),dpi=600)
+        plt.savefig('reports/figures/synth_results_'+modelname+'_'+str(tt),dpi=600)
 # fig.suptitle(name+' mixture model')
 plt.show()
 stop=7
