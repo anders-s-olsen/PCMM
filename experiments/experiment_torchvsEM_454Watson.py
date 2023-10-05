035d7faba109e6c7d54f555c1397ccf55a7794be
Anders <ansol@dtu.dk>
2023-10-05 15:51:47 +0200

    454Watson experiment

---
+++ experiments/experiment_torchvsEM_454Watson.py
@@ -0,0 +1,33 @@
+from src.helper_functions import load_data,run_model_reps_and_save_logliks,parse_input_args
+
+def run_experiment(extraoptions={}):
+    # options pertaining to current experiment
+    options = {}
+    options['tol'] = 1e-8
+    options['num_repl_outer'] = 10
+    options['num_repl_inner'] = 1
+    options['max_iter'] = 100000
+    options['num_subjects'] = 100
+    options['ACG_rank'] = 25 #for ACG and MACG
+    options['outfolder'] = 'data/results/torchvsEM_454Watson_outputs'
+    options.update(extraoptions) #modelname, LR, init, GSR controlled in shell script
+    
+    ## load data, only the first 100 subjects (each with 1200 data points)
+    options['data_type'] = 'fMRI_SchaeferTian454'
+    data_train,data_test,data_test2 = load_data(options=options)
+    p = data_train.shape[1]
+    
+    for K in [2,5,10]:
+        options['experiment_name'] = '454_'+options['init']+'_'+str(options['LR'])+'_p'+str(p)+'_K'+str(K)
+        run_model_reps_and_save_logliks(data_train=data_train,data_test=data_test,data_test2=data_test2,K=K,options=options)
+
+
+if __name__=="__main__":
+    # options['modelname'] = 'ACG_lowrank'
+    # options['LR'] = float(0)
+    # options['init'] = 'unif'
+    # run_experiment(options)
+
+    import sys
+    options=parse_input_args(sys.argv)
+    run_experiment(options)
--- submitfiles/submit_experiment_torchvsEM_454Watson.sh
+++ submitfiles/submit_experiment_torchvsEM_454Watson.sh
@@ -1,16 +1,13 @@
 #!/bin/sh
 inits=("unif" "++" "dc")
-for K in {2..30}
-do
 for LR in 0 1 0 0.1 0.01
 do
 for init in "${inits[@]}"
 do
 
     sed -i '$ d' submitfiles/pyspheremixtures454_template.sh
-    echo "python3 experiments/run_models_454.py Watson $LR $init $K" >> submitfiles/pyspheremixtures454_template.sh
+    echo "python3 experiments/run_models_454.py Watson $LR $init" >> submitfiles/pyspheremixtures454_template.sh
     bsub < submitfiles/pyspheremixtures454_template.sh
 
-done
 done
 done
