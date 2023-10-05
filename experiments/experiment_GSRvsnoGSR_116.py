d2d0c36c6aa29e9fc15468f10db8df19937e770c
Anders <ansol@dtu.dk>
2023-10-05 14:50:00 +0200

    fix?

--- experiments/experiment_GSRvsnoGSR_116.py
+++ experiments/experiment_GSRvsnoGSR_116.py
@@ -1,17 +1,17 @@
 from src.helper_functions import load_data,run_model_reps_and_save_logliks,parse_input_args
 
-# options pertaining to current experiment
-options = {}
-options['tol'] = 1e-8
-options['num_repl_outer'] = 10
-options['num_repl_inner'] = 1
-options['max_iter'] = 100000
-options['num_subjects'] = 100
-options['ACG_rank'] = 25 #for ACG and MACG
-options['outfolder'] = 'data/results/116_GSR_vs_noGSR_outputs'
-
 def run_experiment(extraoptions={}):
+    # options pertaining to current experiment
+    options = {}
+    options['tol'] = 1e-8
+    options['num_repl_outer'] = 10
+    options['num_repl_inner'] = 1
+    options['max_iter'] = 100000
+    options['num_subjects'] = 100
+    options['ACG_rank'] = 25 #for ACG and MACG
+    options['outfolder'] = 'data/results/116_GSR_vs_noGSR_outputs'
     options.update(extraoptions) #modelname, LR, init, GSR controlled in shell script
+    
     ## load data, only the first 100 subjects (each with 1200 data points)
     if options['GSR']==0:
         options['data_type'] = 'fMRI_SchaeferTian116'
